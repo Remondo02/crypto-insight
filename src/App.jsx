d7d066541cc581a9d0cc13a05dba6dea1bf3a770
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-11-28 16:24:34 +0900

    Topbar with theme toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { ColorModeContext, useMode } from "./theme.js"
 import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Routes, Route } from "react-router-dom"
+import { Topbar } from "./global/Topbar.jsx"
+import { Dashboard } from "./pages/Dashboard.jsx"
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -7,7 +10,12 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div>Crypto Insight</div>
+        <main>
+          <Topbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </main>
       </ThemeProvider>
     </ColorModeContext.Provider>
   )
--- src/global/Topbar.jsx
+++ src/global/Topbar.jsx
@@ -1,3 +1,39 @@
+import { IconButton, useTheme } from "@mui/material"
+import Box from "@mui/system/Box"
+import { useContext } from "react"
+import { ColorModeContext, tokens } from "../theme.js"
+import InputBase from "@mui/material/InputBase"
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
+import SearchIcon from "@mui/icons-material/Search"
+
 export function Topbar() {
-  return <div>Topbar</div>
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  const colorMode = useContext(ColorModeContext)
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search"></InputBase>
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <LightModeOutlinedIcon />
+          ) : (
+            <DarkModeOutlinedIcon />
+          )}
+        </IconButton>
+      </Box>
+    </Box>
+  )
 }
