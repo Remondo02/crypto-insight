faf8897118aa47b511af3db12482e23bf164598f
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-11-29 14:10:54 +0900

    Sidebar

--- src/global/Sidebar.jsx
+++ src/global/Sidebar.jsx
@@ -1,3 +1,119 @@
+import { useState } from "react"
+import { Sidebar as ProSideBar, Menu, MenuItem } from "react-pro-sidebar"
+import { Box, IconButton, Typography, useTheme } from "@mui/material"
+import { Link } from "react-router-dom"
+import { tokens } from "./../theme.js"
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined"
+import CurrencyBitcoinOutlinedIcon from "@mui/icons-material/CurrencyBitcoinOutlined"
+import CurrencyExchangeOutlinedIcon from "@mui/icons-material/CurrencyExchangeOutlined"
+import FeedOutlinedIcon from "@mui/icons-material/FeedOutlined"
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined"
+import { Home } from "@mui/icons-material"
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  return (
+    <MenuItem
+      component={<Link to={to} />}
+      active={selected === title}
+      style={{ color: colors.grey[100] }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+    </MenuItem>
+  )
+}
+
 export function Sidebar() {
-  return <div>Sidebar</div>
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [selected, setSelected] = useState("Dashboard")
+
+  return (
+    <ProSideBar
+      rootStyles={{
+        borderRightStyle: "none",
+      }}
+      backgroundColor={`${colors.primary[400]}`}
+      width={"328.9px"}
+      collapsed={isCollapsed}
+    >
+      <Menu
+        iconShape="square"
+        menuItemStyles={{
+          button: ({ active }) => {
+            const baseStyle = {
+              padding: "5px 20px",
+              "&:hover": {
+                color: "#868dfb !important",
+                backgroundColor: "transparent",
+              },
+            }
+            if (active) return { ...baseStyle, color: "#6870fa !important" }
+            return baseStyle
+          },
+        }}
+      >
+        <MenuItem
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+          style={{
+            margin: "10px 0 20px",
+            color: colors.grey[100],
+          }}
+        >
+          {!isCollapsed && (
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              ml="15px"
+            >
+              <Typography variant="h3" color={colors.grey[100]}>
+                CRYPTO INSIGHT
+              </Typography>
+              <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                <MenuOutlinedIcon />
+              </IconButton>
+            </Box>
+          )}
+        </MenuItem>
+
+        <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+          <Item
+            title="Dashboard"
+            to="/"
+            icon={<HomeOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+          <Item
+            title="Crypto Currencies"
+            to="/cryptocurrencies"
+            icon={<CurrencyBitcoinOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+          <Item
+            title="Exchanges"
+            to="/exchanges"
+            icon={<CurrencyExchangeOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+          <Item
+            title="News"
+            to="/news"
+            icon={<FeedOutlinedIcon />}
+            selected={selected}
+            setSelected={setSelected}
+          />
+        </Box>
+      </Menu>
+    </ProSideBar>
+    // </Box>
+  )
 }
