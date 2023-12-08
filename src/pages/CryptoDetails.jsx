d4d38e58beb43030c8892174f475ae728c494dc6
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-12-08 15:27:49 +0900

    Refactorisation cryptodetails

--- src/pages/CryptoDetails.jsx
+++ src/pages/CryptoDetails.jsx
@@ -1,39 +1,16 @@
-import {
-  Box,
-  Typography,
-  useTheme,
-  Grid,
-  Divider,
-  Avatar,
-  Link,
-  List,
-  ListItemButton,
-  ListItemText,
-  ListItem,
-} from "@mui/material"
+import { Box, Grid } from "@mui/material"
 import { Header } from "../components/Header.jsx"
 import HTMLReactParser from "html-react-parser"
 import { useParams } from "react-router-dom"
-import millify from "millify"
 import { useState } from "react"
 import { useGetCryptoDetailsApiQuery } from "../services/cryptoApi.js"
-import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined"
-import BoltOutlinedIcon from "@mui/icons-material/BoltOutlined"
-import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined"
-import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined"
-import TrendingUpOutlinedIcon from "@mui/icons-material/TrendingUpOutlined"
-import CurrencyExchangeOutlinedIcon from "@mui/icons-material/CurrencyExchangeOutlined"
-import InventoryOutlinedIcon from "@mui/icons-material/InventoryOutlined"
-import StopCircleOutlinedIcon from "@mui/icons-material/StopCircleOutlined"
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined"
-import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined"
 
-import { tokens } from "../theme.js"
+import { getGenericStats, getStats } from "../utils/singleUtils.jsx"
+
 import { SearchSelect } from "../components/SearchSelect.jsx"
+import { SingleList } from "../components/SingleList.jsx"
 
 export function CryptoDetails() {
-  const theme = useTheme()
-  const colors = tokens(theme.palette.mode)
   const { coinId } = useParams()
 
   const [timePeriod, setTimePeriod] = useState("7d")
@@ -52,85 +29,8 @@ export function CryptoDetails() {
 
   const time = ["24h", "7d", "30d"]
 
-  const stats = [
-    {
-      title: "Price to USD",
-      value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
-      icon: <AttachMoneyOutlinedIcon />,
-    },
-    {
-      title: "Rank",
-      value: cryptoDetails?.rank,
-      icon: <ThumbUpOutlinedIcon />,
-    },
-    {
-      title: "24h Volume",
-      value: `$ ${
-        cryptoDetails["24hVolume"] && millify(cryptoDetails["24hVolume"])
-      }`,
-      icon: <BoltOutlinedIcon />,
-    },
-    {
-      title: "Market Cap",
-      value: `$ ${
-        cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
-      }`,
-      icon: <MonetizationOnOutlinedIcon />,
-    },
-    {
-      title: "All-time-high(daily avg.)",
-      value: `$ ${
-        cryptoDetails?.allTimeHigh?.price &&
-        millify(cryptoDetails?.allTimeHigh?.price)
-      }`,
-      icon: <EmojiEventsOutlinedIcon />,
-    },
-  ]
-
-  const genericStats = [
-    {
-      title: "Number Of Markets",
-      value: cryptoDetails?.numberOfMarkets,
-      icon: <TrendingUpOutlinedIcon />,
-    },
-    {
-      title: "Number Of Exchanges",
-      value: cryptoDetails?.numberOfExchanges,
-      icon: <CurrencyExchangeOutlinedIcon />,
-    },
-    {
-      title: "Aprroved Supply",
-      value: cryptoDetails?.supply?.confirmed ? (
-        <InventoryOutlinedIcon />
-      ) : (
-        <StopCircleOutlinedIcon />
-      ),
-      icon: <InfoOutlinedIcon />,
-    },
-    {
-      title: "Total Supply",
-      value: `$ ${
-        cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
-      }`,
-      icon: <InfoOutlinedIcon />,
-    },
-    {
-      title: "Circulating Supply",
-      value: `$ ${
-        cryptoDetails?.supply?.circulating &&
-        millify(cryptoDetails?.supply?.circulating)
-      }`,
-      icon: <InfoOutlinedIcon />,
-    },
-  ]
-
-  const toto = {
-    color: "#6870fa",
-    textDecoration: "none",
-    "&:hover": {
-      color: "#868dfb",
-    },
-  }
+  const stats = getStats(cryptoDetails)
+  const genericStats = getGenericStats(cryptoDetails)
 
   return (
     <Box m={3}>
@@ -153,151 +53,25 @@ export function CryptoDetails() {
       <Box>
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={{ xs: 2, md: 3 }}>
-            <Grid item sm={12} md={6} lg={4}>
-              <Box>
-                <Box marginBottom={2}>
-                  <Typography
-                    sx={{ fontWeight: "bold" }}
-                    variant="h4"
-                    color={colors.greenAccent[500]}
-                    mb={1}
-                  >
-                    {cryptoDetails.name} Value Statistics
-                  </Typography>
-                  <Typography variant="h5" color={colors.grey[100]}>
-                    An overview showing the stats of {cryptoDetails.name}
-                  </Typography>
-                </Box>
-                <Box backgroundColor={colors.primary[400]}>
-                  {stats.map(({ icon, title, value }) => (
-                    <Box key={title}>
-                      <Box
-                        display="flex"
-                        justifyContent="space-between"
-                        alignItems="center"
-                        p={2}
-                      >
-                        <Box display="flex" alignItems="inherit">
-                          <Avatar
-                            sx={{ bgcolor: colors.greenAccent[400], mr: 2 }}
-                          >
-                            {icon}
-                          </Avatar>
-                          <Typography variant="body1" color={colors.grey[100]}>
-                            {title}
-                          </Typography>
-                        </Box>
-                        <Typography
-                          component="div"
-                          fontWeight="bold"
-                          sx={{ color: colors.grey[100] }}
-                        >
-                          {value}
-                        </Typography>
-                      </Box>
-                      <Divider />
-                    </Box>
-                  ))}
-                </Box>
-              </Box>
-            </Grid>
-            <Grid item sm={12} md={6} lg={4}>
-              <Box>
-                <Box marginBottom={2}>
-                  <Typography
-                    sx={{ fontWeight: "bold" }}
-                    variant="h4"
-                    color={colors.greenAccent[500]}
-                    mb={1}
-                  >
-                    Other Statistics
-                  </Typography>
-                  <Typography variant="h5" color={colors.grey[100]}>
-                    An overview showing the stats of all cryptocurrencies
-                  </Typography>
-                </Box>
-                <Box backgroundColor={colors.primary[400]}>
-                  {genericStats.map(({ icon, title, value }) => (
-                    <Box key={title}>
-                      <Box
-                        display="flex"
-                        justifyContent="space-between"
-                        alignItems="center"
-                        p={2}
-                      >
-                        <Box display="flex" alignItems="inherit">
-                          <Avatar
-                            sx={{ bgcolor: colors.greenAccent[400], mr: 2 }}
-                          >
-                            {icon}
-                          </Avatar>
-                          <Typography variant="body1" color={colors.grey[100]}>
-                            {title}
-                          </Typography>
-                        </Box>
-                        <Typography
-                          component="div"
-                          fontWeight="bold"
-                          sx={{ color: colors.grey[100] }}
-                        >
-                          {value}
-                        </Typography>
-                      </Box>
-                      <Divider />
-                    </Box>
-                  ))}
-                </Box>
-              </Box>
-            </Grid>
-            <Grid item sm={12} md={12} lg={4}>
-              <Box>
-                <Box marginBottom={2}>
-                  <Typography
-                    sx={{ fontWeight: "bold" }}
-                    variant="h4"
-                    color={colors.greenAccent[500]}
-                    mb={1}
-                  >
-                    {cryptoDetails.name} Links
-                  </Typography>
-                  <Typography variant="h5" color={colors.grey[100]}>
-                    Various links related to {cryptoDetails.name}
-                  </Typography>
-                </Box>
-                <Box backgroundColor={colors.primary[400]}>
-                  {cryptoDetails.links.map(({ name, type, url }) => (
-                    <Link
-                      href={url}
-                      underline="none"
-                      target="_blank"
-                      rel="noreferrer"
-                      sx={{
-                        color: colors.grey[100],
-                        ".MuiListItemText-root": {
-                          display: "flex",
-                          justifyContent: "space-between",
-                        },
-                        ".MuiListItemText-secondary": {
-                          fontSize: theme.typography.body1,
-                          color: colors.greenAccent[500],
-                        },
-                      }}
-                    >
-                      <ListItem
-                        key={name}
-                        component="div"
-                        disablePadding
-                        divider={true}
-                      >
-                        <ListItemButton>
-                          <ListItemText primary={type} secondary={name} />
-                        </ListItemButton>
-                      </ListItem>
-                    </Link>
-                  ))}
-                </Box>
-              </Box>
-            </Grid>
+            <SingleList
+              title={`${cryptoDetails.name} Value Statistics`}
+              subtitle={`An overview showing the stats of ${cryptoDetails.name}`}
+              name={cryptoDetails.name}
+              stats={stats}
+            />
+
+            <SingleList
+              title="Other Statistics"
+              subtitle="An overview showing the stats of all cryptocurrencies"
+              name={cryptoDetails.name}
+              genericStats={genericStats}
+            />
+
+            <SingleList
+              title={`${cryptoDetails.name} Links`}
+              subtitle={` Various links related to ${cryptoDetails.name}`}
+              links={cryptoDetails.links}
+            />
           </Grid>
         </Box>
       </Box>
---
+++ src/utils/singleUtils.jsx
@@ -0,0 +1,105 @@
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined"
+import GitHubIcon from "@mui/icons-material/GitHub"
+import RedditIcon from "@mui/icons-material/Reddit"
+import TelegramIcon from "@mui/icons-material/Telegram"
+import TwitterIcon from "@mui/icons-material/Twitter"
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined"
+import YouTubeIcon from "@mui/icons-material/YouTube"
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined"
+import BoltOutlinedIcon from "@mui/icons-material/BoltOutlined"
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined"
+import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined"
+import TrendingUpOutlinedIcon from "@mui/icons-material/TrendingUpOutlined"
+import CurrencyExchangeOutlinedIcon from "@mui/icons-material/CurrencyExchangeOutlined"
+import InventoryOutlinedIcon from "@mui/icons-material/InventoryOutlined"
+import StopCircleOutlinedIcon from "@mui/icons-material/StopCircleOutlined"
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined"
+import AttachMoneyOutlinedIcon from "@mui/icons-material/AttachMoneyOutlined"
+
+import millify from "millify"
+
+export function getIcon(type) {
+  if (type === "github") return <GitHubIcon />
+  if (type === "reddit") return <RedditIcon />
+  if (type === "telegram") return <TelegramIcon />
+  if (type === "twitter") return <TwitterIcon />
+  if (type === "facebook") return <FacebookOutlinedIcon />
+  if (type === "youtube") return <YouTubeIcon />
+  return <LanguageOutlinedIcon />
+}
+
+export function getStats(cryptoDetails) {
+  return [
+    {
+      title: "Price to USD",
+      value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
+      icon: <AttachMoneyOutlinedIcon />,
+    },
+    {
+      title: "Rank",
+      value: cryptoDetails?.rank,
+      icon: <ThumbUpOutlinedIcon />,
+    },
+    {
+      title: "24h Volume",
+      value: `$ ${
+        cryptoDetails["24hVolume"] && millify(cryptoDetails["24hVolume"])
+      }`,
+      icon: <BoltOutlinedIcon />,
+    },
+    {
+      title: "Market Cap",
+      value: `$ ${
+        cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
+      }`,
+      icon: <MonetizationOnOutlinedIcon />,
+    },
+    {
+      title: "All-time-high(daily avg.)",
+      value: `$ ${
+        cryptoDetails?.allTimeHigh?.price &&
+        millify(cryptoDetails?.allTimeHigh?.price)
+      }`,
+      icon: <EmojiEventsOutlinedIcon />,
+    },
+  ]
+}
+
+export function getGenericStats(cryptoDetails) {
+  return [
+    {
+      title: "Number Of Markets",
+      value: cryptoDetails?.numberOfMarkets,
+      icon: <TrendingUpOutlinedIcon />,
+    },
+    {
+      title: "Number Of Exchanges",
+      value: cryptoDetails?.numberOfExchanges,
+      icon: <CurrencyExchangeOutlinedIcon />,
+    },
+    {
+      title: "Aprroved Supply",
+      value: cryptoDetails?.supply?.confirmed ? (
+        <InventoryOutlinedIcon />
+      ) : (
+        <StopCircleOutlinedIcon />
+      ),
+      icon: <InfoOutlinedIcon />,
+    },
+    {
+      title: "Total Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
+      }`,
+      icon: <InfoOutlinedIcon />,
+    },
+    {
+      title: "Circulating Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.circulating &&
+        millify(cryptoDetails?.supply?.circulating)
+      }`,
+      icon: <InfoOutlinedIcon />,
+    },
+  ]
+}
