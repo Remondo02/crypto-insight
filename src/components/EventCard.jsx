90b5d223833c45e7e08dd27bb8ecdd5d15debd2a
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-12-16 15:10:07 +0900

    Add cryptoEvents list

---
+++ src/components/EventCard.jsx
@@ -0,0 +1,93 @@
+import {
+    Box,
+    Grid,
+    Card,
+    CardMedia,
+    CardContent,
+    CardActionArea,
+    Typography,
+    useTheme,
+  } from "@mui/material"
+  
+  import { Link } from "react-router-dom"
+  import { tokens } from "../theme.js"
+  import moment from "moment"
+  
+  function EventCardWrapper({ url, children }) {
+    return (
+      <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+        {url ? (
+          <Link
+            to={url}
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            {children}
+          </Link>
+        ) : (
+          { children }
+        )}
+      </Grid>
+    )
+  }
+  
+  export function EventCard({
+    title,
+    url,
+    urlToImage,
+    description,
+    author,
+    publishedAt,
+  }) {
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+  
+    return (
+      <EventCardWrapper url={url}>
+        <Card
+          sx={{
+            backgroundColor: colors.primary[400],
+          }}
+        >
+          <CardActionArea>
+            {urlToImage && (
+              <CardMedia
+                component="img"
+                height="140"
+                image={urlToImage}
+                alt={title ?? ""}
+              />
+            )}
+            <CardContent>
+              {title && (
+                <Typography gutterBottom variant="h4" color={colors.grey[100]}>
+                  {title}
+                </Typography>
+              )}
+              {description && (
+                <Typography variant="body1" color="text.secondary">
+                  {description.length > 100
+                    ? `${description.substring(0, 100)} ...`
+                    : description}
+                </Typography>
+              )}
+              <Box mt={2} display="flex" justifyContent="space-between">
+                {author && (
+                  <Typography variant="body2" color={colors.grey[100]}>
+                    {author}
+                  </Typography>
+                )}
+                {publishedAt && (
+                  <Typography variant="body2" color={colors.grey[100]}>
+                    {moment(publishedAt).startOf("ss").fromNow()}
+                  </Typography>
+                )}
+              </Box>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </EventCardWrapper>
+    )
+  }
+  
--- src/pages/CryptoEvents.jsx
+++ src/pages/CryptoEvents.jsx
@@ -2,35 +2,33 @@ import { formatDate } from "@fullcalendar/core"
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
-import interactionPlugin from '@fullcalendar/interaction'
+import interactionPlugin from "@fullcalendar/interaction"
 
 import {
   useGetCryptoEventsApiQuery,
   useGetCryptoEventsCoinsApiQuery,
 } from "../services/cryptoEventsApi.js"
 import { AlertMessage } from "../components/AlertMessage.jsx"
-import { Box, useTheme } from "@mui/material"
+import {
+  Box,
+  Card,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material"
 import { Header } from "../components/Header.jsx"
 import { SearchSelect } from "../components/SearchSelect.jsx"
 import { Loader } from "../components/Loader.jsx"
 import { useState } from "react"
 import { tokens } from "../theme.js"
 
-
-function renderEventContent(eventInfo) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  )
-}
-
 export function CryptoEvents() {
   const theme = useTheme()
   const colors = tokens(theme.palette.mode)
   const [search, setSearch] = useState("btc-bitcoin")
-  const schedulerTheme = theme.palette.mode === "dark" ? "e-dark-mode" : ""
 
   const {
     data: cryptoCoins,
@@ -77,57 +75,86 @@ export function CryptoEvents() {
       start: event.date,
       formatTime: new Date(event.date).getTime(),
       end: endDate ?? event.date,
-      // allDay: false,
+      allDay: false,
     })
   }
 
-  const latestEvent = convertedData.reduce(
-    (acc, val) =>
-      acc.formatTime > val.formatTime ? acc.formatTime : val.formatTime,
-    0
-  )
-
-  console.log(convertedData)
+  console.log(cryptoEvents)
 
   return (
-    <div className={schedulerTheme}>
-      <Box m={3}>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Header
-            title="CRYPTO CURRENCIES"
-            subtitle="All reccent events related to currencies"
-          />
-        </Box>
-        <Box mb={3}>
-          <SearchSelect
-            search={search}
-            optionValue={coins}
-            onSearchChange={setSearch}
-          />
-        </Box>
-        {!!latestEvent && (
-          <Box>
-            <div className="demo-app-main">
-              <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                headerToolbar={{
-                  left: "prev,next,today",
-                  center: "title",
-                  right: "dayGridMonth,timeGridWeek,timeGridDay",
-                }}
-                initialView="dayGridMonth"
-                editable={false}
-                selectable={false}
-                selectMirror={true}
-                dayMaxEvents={true}
-                events={convertedData}
-                // eventContent={renderEventContent}
-              />
-            </div>
-          </Box>
-        )}
+    <Box m={3}>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header
+          title="CRYPTO CURRENCIES"
+          subtitle="All informations related to currencies"
+        />
       </Box>
-    </div>
+      {convertedData && (
+        <Box>
+          <Box mb={3}>
+            <SearchSelect
+              search={search}
+              optionValue={coins}
+              onSearchChange={setSearch}
+            />
+          </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid
+              container
+              spacing={{ xs: 2, md: 3 }}
+              columns={{ xs: 4, sm: 4, md: 8, lg: 12, xl: 16 }}
+            >
+              <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Box backgroundColor={colors.primary[400]} p={2}>
+                  <Typography variant="h5">Events</Typography>
+                  <List>
+                    {convertedData.map((event) => (
+                      <ListItem
+                        key={event.id}
+                        sx={{
+                          backgroundColor: colors.greenAccent[500],
+                          margin: "10px 0",
+                          borderRadius: "2px",
+                        }}
+                      >
+                        <ListItemText
+                          primary={event.title}
+                          secondary={
+                            <Typography>
+                              {formatDate(event.start, {
+                                year: "numeric",
+                                month: "short",
+                                day: "numeric",
+                              })}
+                            </Typography>
+                          }
+                        ></ListItemText>
+                      </ListItem>
+                    ))}
+                  </List>
+                </Box>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} lg={8} xl={12}>
+                <FullCalendar
+                  plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                  headerToolbar={{
+                    left: "prev,next,today",
+                    center: "title",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay",
+                  }}
+                  initialView="dayGridMonth"
+                  editable={false}
+                  selectable={true}
+                  selectMirror={true}
+                  dayMaxEvents={true}
+                  events={convertedData}
+                />
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      )}
+    </Box>
   )
 }
 
