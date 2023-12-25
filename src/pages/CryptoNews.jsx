8fdb1402a58fff86b0d488e74cce5470c0465377
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-12-26 03:43:20 +0900

    Modif archi fetch

--- src/pages/CryptoNews.jsx
+++ src/pages/CryptoNews.jsx
@@ -26,7 +26,7 @@ export default function CryptoNews({ simplified }) {
     data: cryptoNews,
     error: errorNews,
     isLoading: isLoadingNews,
-    isFetching: isFetchingNews
+    isFetching: isFetchingNews,
   } = useGetCryptoNewsApiQuery({
     newsCategory: search,
     count: count,
@@ -69,35 +69,44 @@ export default function CryptoNews({ simplified }) {
             {isFetchingNews ? (
               <Loader />
             ) : (
-              <Box sx={{ flexGrow: 1 }}>
-                <Grid
-                  container
-                  spacing={{ xs: 2, md: 3 }}
-                  columns={{ xs: 4, sm: 4, md: 8, lg: 12, xl: 16 }}
-                >
-                  {cryptoNews.value.map(
-                    ({
-                      name,
-                      url,
-                      image,
-                      description,
-                      provider,
-                      datePublished,
-                    }) => (
-                      <NewsCard
-                        simplified={simplified}
-                        key={name}
-                        title={name}
-                        url={url}
-                        image={image}
-                        description={description}
-                        provider={provider}
-                        datePublished={datePublished}
-                      />
-                    )
-                  )}
-                </Grid>
-              </Box>
+              <>
+                {cryptoNews.value.length > 0 ? (
+                  <Box sx={{ flexGrow: 1 }}>
+                    <Grid
+                      container
+                      spacing={{ xs: 2, md: 3 }}
+                      columns={{ xs: 4, sm: 4, md: 8, lg: 12, xl: 16 }}
+                    >
+                      {cryptoNews.value.map(
+                        ({
+                          name,
+                          url,
+                          image,
+                          description,
+                          provider,
+                          datePublished,
+                        }) => (
+                          <NewsCard
+                            simplified={simplified}
+                            key={name}
+                            title={name}
+                            url={url}
+                            image={image}
+                            description={description}
+                            provider={provider}
+                            datePublished={datePublished}
+                          />
+                        )
+                      )}
+                    </Grid>
+                  </Box>
+                ) : (
+                  <AlertMessage
+                    type="info"
+                    errors={`No news available for ${cryptoNews.queryContext.originalQuery}.`}
+                  />
+                )}
+              </>
             )}
           </Box>
         )
