648010f4d460fb78c59c4ef27f28ad80e56c0871
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-12-25 18:59:29 +0900

    Modif archi fetch CryptoCurrencies

--- src/pages/CryptoCurrencies.jsx
+++ src/pages/CryptoCurrencies.jsx
@@ -8,21 +8,14 @@ export default function CryptoCurrencies({ simplified }) {
   const {
     data: cryptosList,
     error,
-    isLoading,
     isFetching,
   } = useGetCryptoApiQuery(count)
 
   const [search, setSearch] = useState("")
 
-  if (isLoading || isFetching) {
-    return <Loader />
-  }
+  const styles = { height: "inherit" }
 
-  if (error) {
-    return <AlertMessage type="error" errors={error} />
-  }
-
-  const cryptos = cryptosList?.data?.coins
+  const cryptos = cryptosList?.data?.coins || []
 
   const visibleItems = cryptos.filter((coin) => {
     if (search && !coin.name.toLowerCase().includes(search.toLowerCase())) {
@@ -32,7 +25,7 @@ export default function CryptoCurrencies({ simplified }) {
   })
 
   return (
-    <Box sx={!simplified ? { margin: 3 } : {}}>
+    <Box sx={!simplified ? { margin: 3, ...styles } : { ...styles }}>
       {!simplified && (
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <Header
@@ -41,22 +34,28 @@ export default function CryptoCurrencies({ simplified }) {
           />
         </Box>
       )}
-      {visibleItems && (
-        <Box>
-          {!simplified && <Search search={search} onSearchChange={setSearch} />}
-
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid
-              container
-              spacing={{ xs: 2, md: 3 }}
-              columns={{ xs: 4, sm: 4, md: 8, lg: 12, xl: 16 }}
-            >
-              {visibleItems.map((currency, i) => (
-                <CryptoCard key={i} currency={currency} />
-              ))}
-            </Grid>
-          </Box>
-        </Box>
+      {error && <AlertMessage type="error" errors={error} />}
+      {isFetching ? (
+        <Loader />
+      ) : (
+        visibleItems && (
+          <>
+            {!simplified && (
+              <Search search={search} onSearchChange={setSearch} />
+            )}
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid
+                container
+                spacing={{ xs: 2, md: 3 }}
+                columns={{ xs: 4, sm: 4, md: 8, lg: 12, xl: 16 }}
+              >
+                {visibleItems.map((currency, i) => (
+                  <CryptoCard key={i} currency={currency} />
+                ))}
+              </Grid>
+            </Box>
+          </>
+        )
       )}
     </Box>
   )
