44b84e224be18e33f4ebc96456d80572f3bd04af
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2023-12-27 12:40:58 +0900

    Remove last section mb on Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -90,7 +90,7 @@ export default function Dashboard() {
         />
         <CryptoCurrencies simplified />
       </Box>
-      <Box mb={7}>
+      <Box>
         <SectionHeader
           title="Latest Crypto News"
           to="/news"
