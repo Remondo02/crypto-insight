3af4d5f039ee0c127005136e33244493478d52be
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-01-18 11:34:42 +0900

    Add utils to reduce jsx files

---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+export { chartJsData, chartJsSettings } from "./chartJs.js"
+export { default as getCoins } from "./coins.js"
+export { default as eventsData } from "./eventsData.js"
+export { default as errorMessage } from "./errorMessage.js"
